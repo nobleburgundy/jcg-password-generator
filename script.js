2934cb84ddb6ed6fe080148085764a07272be8af
James Gould <nobleburgundy@gmail.com>
2020-10-14 19:07:31 -0500

    Added function to get user input and check if it's valid.

--- script.js
+++ script.js
@@ -10,5 +10,46 @@ function writePassword() {
 
 }
 
+// Get password parameter input from the user of at least one:
+// Upper Case
+// Lower Case
+// Speical Character
+// and Specify length
+function getPasswordCharactersInput() {
+  // Initialize password condition variables to false
+  let atLeastOne = false;
+  let lowerCase = false;
+  let upperCase = false;
+  let specialChars = false;
+  // Need at least one of the following
+  while (!atLeastOne) {
+    upperCase = confirm("Would you like to include uppercase characters?");
+    lowerCase = confirm("Would you like to include lowercase characters?");
+    specialChars = confirm("Would you like to include special characters?");
+    if (upperCase || lowerCase || specialChars) {
+      atLeastOne = true;
+    }
+  }
+
+  // Get password length
+  let acceptableLength = false;
+  let passwordLength = prompt("How long would you like your password to be (min 8 max 128)?");
+  while (!acceptableLength) {
+    if (parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128 || isNaN(parseInt(passwordLength))) {
+      passwordLength = prompt("Please try again and select between 8 and 128:");
+    } else {
+      acceptableLength = true;
+    }
+  }
+
+  passwordObj = {
+    "length": parseInt(passwordLength),
+    "lowerCase": lowerCase,
+    "upperCase": upperCase,
+    "specialCharacers": specialChars
+  }
+  return passwordObj;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
